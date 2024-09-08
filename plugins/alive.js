d1f265bd940666158fbc12c567ad8b23571f6a19
rudhachu <120007671+rudhachu@users.noreply.github.com>
2024-09-08 12:49:38 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,32 +1,71 @@
-const { Rudhra, mode, formatTime } = require('../lib/');
-
+const { Rudhra, mode,formatTime } = require('../lib/');
+const config = require("../config");
  Rudhra({
-	pattern: 'ping ?(.*)',
-	fromMe: mode,
-	desc: 'Rudhra response in second.',
-	type: 'info'
+    pattern: "ping",
+    fromMe: mode,
+    desc: "Bot response in second.",
+    type: "info",
 }, async (message, match, client) => {
-	var start = new Date().getTime();
-	var msg = await message.reply('*Pinging...*');
-	var end = new Date().getTime();
-	var responseTime = end - start;
-	await msg.edit(`*Pong!*\nLatency: ${responseTime}ms`);
+    let thumbnail = 'https://raw.githubusercontent.com/rudhra-prh/media/main/image/rudhra2.jpg';
+    let vcardMessage = {
+        key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' },
+        message: {
+            contactMessage: {
+                displayName: config.BOT_NAME,
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:Rudhra-3\nitem1.TEL;waid=${message.sender.split('@')[0]}:${message.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+            },
+        },
+    };
+    
+    const start = new Date().getTime();
+    
+    // Send the initial "Pinging..." message
+    let pingMsg = await client.sendMessage(message.jid, { text: 'Pinging...' }, { quoted: vcardMessage });
+
+    const end = new Date().getTime();
+    const ms = end - start;
+
+    await client.relayMessage(
+        message.jid,
+        {
+            protocolMessage: {
+                key: pingMsg.key,
+                type: 14,
+                editedMessage: {
+                    conversation: `Pong ${ms}ms`,
+                },
+            },
+        },
+        {}
+    );
 });
 
 Rudhra({
-	pattern: 'jid',
+	pattern: "jid ?(.*)",
 	fromMe: mode,
 	desc: 'To get remoteJid',
-	type: 'whatsapp'
+	type: 'info'
 }, async (message) => {
 	await message.send(message.mention[0] ? message.mention[0] : message.quoted ? message.quoted.sender : message.chat)
 });
-
 Rudhra({
-	pattern: 'runtime',
+	pattern: 'runtime$',
 	fromMe: mode,
-	desc: 'Get Rudhras runtime',
+	desc: 'Get bots runtime',
 	type: 'info'
 }, async (message, match, client) => {
 	await message.reply(formatTime(process.uptime()));
-})
+});
+Rudhra(
+	{
+		pattern: 'save ?(.*)',
+		fromMe: mode,
+		desc: 'forward replied msg to u',
+		type: 'misc',
+	},
+	async (message, match) => {
+		if (!message.reply_message)
+			return await message.send('*Reply to a message*');
+		await message.forwardMessage(message.sender, message.quoted.data);
+	}
+);
